1930daf6f397832ec628df56163fc216b852291d
LiuTop <137952103+LiuTop@users.noreply.github.com>
2024-02-05 23:35:09 +0300

    Add files via upload

---
+++ Топоркова_java (задание 3).java
@@ -0,0 +1,10 @@
+public class Example3{
+     public static void main(String args[])
+     {
+    	 int [] a = {2, 4, 6, 8, 10};
+
+    	    for (int i = 0; i < a.length; i++) {
+    	        System.out.printf( "%d\n", a[i] );
+    	    }
+    	}
+     }
