77d67a1d32d81509db004321c51a3753b999b0aa
LiuTop <137952103+LiuTop@users.noreply.github.com>
2024-02-05 23:34:40 +0300

    Add files via upload

---
+++ Топоркова_java (задание 2).java
@@ -0,0 +1,11 @@
+public class Example2{
+	public static void main(String args[]) {
+	  String a = "Java — объектно-ориентированный язык программирования";
+      String b = new String("Java — объектно-ориентированный язык программирования");
+   if(a.equals(b)){
+	   System.out.println("Строки идентичны");
+   }else{
+       System.out.println("Строки не идентичны");
+      }
+}
+}
